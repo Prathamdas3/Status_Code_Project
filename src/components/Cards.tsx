1477fa728f0a8b60b3732b1a960e56baab3268b5
Pratham Das <prathamdas003@gmail.com>
2024-08-24 12:53:45 +0530

    chore(app): added card

---
+++ src/components/Cards.tsx
@@ -0,0 +1,15 @@
+'use client'
+import { Card, CardDescription } from './ui/card'
+import { Button } from './ui/button'
+import Image from 'next/image'
+
+export default function Cards() {
+  return (
+    <section>
+      {/* <div>
+        <Image src="" alt="This is test demo" />
+      </div> */}
+      <Button>Click here</Button>
+    </section>
+  )
+}
